7f3cb740f6b0d1f10184c48fe331c80bf3278ee0
mystique09 <knightkrypto7@gmail.com>
2022-01-17 14:47:38 +0800

    add divider component

---
+++ src/lib/components/Divider.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let text;
+</script>
+
+<div class="container">
+	<div class:divider={text} class:full={!text}>
+		{#if text}
+			<span>{text}</span>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.container {
+		@apply relative w-full;
+	}
+	.divider {
+		@apply m-6 font-thin relative text-center;
+	}
+	.full {
+		@apply w-full h-[1px] bg-gray-300;
+	}
+	.divider::before,
+	.divider::after {
+		content: '';
+		@apply absolute h-[1px] w-[105px] bg-gray-300;
+	}
+	.divider::before {
+		@apply left-2 top-3;
+	}
+	.divider::after {
+		@apply right-2 top-3;
+	}
+</style>
