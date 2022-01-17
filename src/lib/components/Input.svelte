a6901601373245c467b367c5fb3978b5b9c32bc8
mystique09 <knightkrypto7@gmail.com>
2022-01-17 14:48:30 +0800

    add custom input component

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let name;
+	export let id;
+	export let placeholder;
+	export let label;
+</script>
+
+<label for={name}>{label}</label>
+<div class="input-container">
+	<input {placeholder} type="text" {name} {id} />
+</div>
+
+<style>
+	.input-container {
+		@apply mb-6;
+	}
+	input {
+		@apply rounded-md outline outline-gray-300 py-4 px-2 w-full;
+	}
+	label {
+		@apply font-bold text-sm;
+	}
+</style>
