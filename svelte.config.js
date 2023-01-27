d0fef92e3f0ad88ad05d3784e60388ed14370cf8
mystique09 <knightkrypto7@gmail.com>
2023-01-28 02:03:44 +0800

    chore: postcss

--- svelte.config.js
+++ svelte.config.js
@@ -5,13 +5,12 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({})],
+	preprocess: [preprocess({
+        postcss: true
+    })],
 
 	kit: {
 		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
 	}
 };
 
